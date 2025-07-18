b326f3e6e208d971aad56135a7763154e84ad359
matwi <matwic103@gmail.com>
2025-07-18 18:15:07 +0200

    Removing testing code

--- src/main/java/com/petrolpark/mobeffects/SimpleMobEffect.java
+++ src/main/java/com/petrolpark/mobeffects/SimpleMobEffect.java
@@ -1,19 +1,11 @@
 package com.petrolpark.mobeffects;
 
-import com.petrolpark.Petrolpark;
-import com.petrolpark.shadereffects.IShaderEffect;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
-public class SimpleMobEffect extends MobEffect implements IShaderEffect {
+public class SimpleMobEffect extends MobEffect {
 
     protected SimpleMobEffect(MobEffectCategory category, int color) {
         super(category, color);
-    };
-
-    @Override
-    public ResourceLocation getShader() {
-        return Petrolpark.asResource("shaders/post/lead_poisoning.json");
     }
-};
+}
